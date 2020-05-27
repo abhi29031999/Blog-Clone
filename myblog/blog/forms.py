2b574a030b86f8beaf4dd3638e9236ce7e1375ff
abhi29031999 <65550326+abhi29031999@users.noreply.github.com>
2020-05-27 11:13:21 +0530

    Update forms.py

--- myblog/blog/forms.py
+++ myblog/blog/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from blog.models import Post,Comment
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
 
+    class Meta():
+        model = User
+        fields = ('username','password')
 class PostForm(forms.ModelForm):
 
     class Meta():
