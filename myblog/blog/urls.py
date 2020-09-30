083b38e26bfa157e47b2d2eb7d1fdda11fd67e33
abhi29031999 <65550326+abhi29031999@users.noreply.github.com>
2020-09-30 10:41:39 +0530

    Update urls.py

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$',views.PostListView.as_view(),name='post_list'),
     url(r'^about/$',views.AboutView.as_view(),name='about'),
-    url(r'^about/$',views.AboutView.as_view(),name='about'),
+    url(r'^contact/$',views.ContactView.as_view(),name='contact'),
     url(r'^post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
     url(r'^post/new/$', views.CreatePostView.as_view(), name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
